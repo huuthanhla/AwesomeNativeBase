81a88230197c3d0332a8debd7dce6b618d47e489
Thành Lã <thanhlh@lifetimetech.vn>
2018-08-08 23:03:43 +0700

    AwesomeNativeBase

---
+++ App.js
@@ -0,0 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, Badge } from 'native-base';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      appTab: true,
+      cameraTab: false,
+      negativeTab: false,
+      contactTab: false
+    };
+  }
+
+  toggleAppTab() {
+    this.setState({
+      appTab: true,
+      cameraTab: false,
+      negativeTab: false,
+      contactTab: false
+    });
+  }
+
+  toggleCameraTab() {
+    this.setState({
+      appTab: false,
+      cameraTab: true,
+      negativeTab: false,
+      contactTab: false
+    });
+  }
+
+  toggleNegativeTab() {
+    this.setState({
+      appTab: false,
+      cameraTab: false,
+      negativeTab: true,
+      contactTab: false
+    });
+  }
+
+  toggleContactTab() {
+    this.setState({
+      appTab: false,
+      cameraTab: false,
+      negativeTab: false,
+      contactTab: true
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button active={this.state.appTab} onPress={()=> this.toggleAppTab()} badge vertical>
+              <Badge><Text>2</Text></Badge>
+              <Icon name="apps" />
+              <Text>Apps</Text>
+            </Button>
+            <Button active={this.state.cameraTab} onPress={()=> this.toggleCameraTab()} vertical>
+              <Icon name="camera" />
+              <Text>Camera</Text>
+            </Button>
+            <Button active={this.state.negativeTab} onPress={()=> this.toggleNegativeTab()} badge vertical>
+              <Badge ><Text>51</Text></Badge>
+              <Icon active name="navigate" />
+              <Text>Navigate</Text>
+            </Button>
+            <Button active={this.state.contactTab} onPress={()=> this.toggleContactTab()} vertical>
+              <Icon name="person" />
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
